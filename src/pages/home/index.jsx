7d54bda7164559126f091649a7d67a0e5b8a9110
Owen Sullivan <multiojuice@gmail.com>
2019-02-10 02:28:43 -0500

    Added a ton of stuff

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { CenteredContainer } from '../../components/basicStyledComponents';
+import Post from '../../components/post';
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [
+                {
+                    User: {
+                        username: 'multio',
+                        picture: ''
+                    },
+                    Post: {
+                        host: 'twitter',
+                        source: 'url'
+                    }
+                },
+                {
+                    User: {
+                        username: 'skyler',
+                        picture: ''
+                    },
+                    Post: {
+                        host: 'twitter',
+                        source: 'url'
+                    }
+                }
+            ]
+        };
+    }
+
+
+    renderPosts() {
+        return this.state.posts.map((item, index) => 
+        <Post
+            {...item} 
+        />);
+    }
+
+
+    render() {
+        return (
+            <CenteredContainer>
+                {this.renderPosts()}
+            </CenteredContainer>
+        );
+    };
+}
+
+export default Home;
